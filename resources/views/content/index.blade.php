46e60503f760c1dfe5ffcb4a83b5bd7b7af6f26f
endjiansyah <endji1507@gmail.com>
2023-08-03 15:07:43 +0700

    (~)show content berita in index

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -31,27 +31,46 @@
 
     <section id="berita">
         <div class="container">
-            <div class="title">
+            <div class="title" id="bacaberita">
                 <h2>Berita</h2>
                 <p>Berita terbaru SMA Negeri 1 Jepara</p>
             </div>
-            <div class="card-box">
-                
-                @foreach ($berita as $item)    
-                <div class="card">
-                    <img src="{{ $item->image != ''? $item->image : './assets/logosmansara.png' }}" alt="{{ $item->title }}">
-                    <div class="text">
-                        {{ $item->updated_at }}
+
+            <div x-data="{title:'',body:'',waktu:'',image:'',show:false}">
+                <div x-show="show" class="card mb-8 md:mx-2 lg:mx-4 rounded-xl bg-blue-100 p-4 md:p-6 lg:p-8">
+                    <img x-show="image != ''" x-bind:src=" image != ''? image : './assets/logosmansara.png'" alt="title">
+                    <div class="">
+                        <p x-text="waktu"></p>
                             <div class="line">
                             </div>
-                            <h3>
-                                {{ $item->title }}
-                            </h3>
-                            <a href="#!" class="">Continue Reading</a>
+                            <h3 x-text="title"></h3>
+                            <hr class="mb-2">
+                            <p x-text=body></p>
+                            <hr class="mt-3">
+                            <button x-on:cklick="show = 'false'" class="par-c text-red-500 hover:text-red-600">Tutup Berita</button>
                     </div>
                 </div>
-                @endforeach
-
+                
+                <hr class="mb-2">
+                
+                <div class="card-box">
+                
+                    @foreach ($berita as $item)    
+                    <div class="card">
+                        <img src="{{ $item->image != ''? $item->image : './assets/logosmansara.png' }}" alt="{{ $item->title }}">
+                        <div class="text">
+                            {{ $item->updated_at }}
+                                <div class="line">
+                                </div>
+                                <h3>
+                                    {{ $item->title }}
+                                </h3>
+                                <button x-on:click="body = '{{$item['body']}}',title = '{{$item['title']}}',image = '{{$item['image']}}',time = '{{$item['updated_at']}}',show='true'"  onclick="window.location.href='#bacaberita'">Continue Reading</button>
+                        </div>
+                    </div>
+                    @endforeach
+                    
+                </div>
             </div>
         </div>
     </section>
