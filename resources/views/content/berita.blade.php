80ad71b17e48024f90e377b193f56ecc7268faf4
endjiansyah <endji1507@gmail.com>
2023-11-10 17:30:54 +0700

    fixing page berita & pengumuman

--- resources/views/content/berita.blade.php
+++ resources/views/content/berita.blade.php
@@ -2,59 +2,62 @@
 
 @section('content')
 
-    <section id="berita" class="bg-white py-24">
-        <div class="container min-h-[75vh]">
-            <div class="title">
-                <h2 id="bacaberita">Berita</h2>
-                <p>Berita SMA Negeri 1 Jepara</p>
+<section id="berita" class="py-40 bg-gray-100">
+    <div class="container">
+        <div class="title">
+            <h2>Berita</h2>
+            <p>Berita terbaru SMA Negeri 1 Jepara</p>
+        </div>
+
+        <div x-data="{id:'',title:'',body:'',time:'',image:'',show:'false'}">
+            
+            
+            <div x-show="show == 'true'" class="card my-8 md:mx-2 lg:mx-4 rounded-xl bg-white border shadow-xl" id="bacaberita">
+
+                <div class="flex justify-between w-full p-4">
+                    <p x-text="time"></p>
+                    <button x-on:click="show = 'false', id=''" class="bg-red-500 hover:bg-red-600 text-white text-center w-7 h-7 rounded-full">x</button>
+                </div>
+                <div class=" p-4 md:p-6">
+                    <div class="w-full flex justify-center">
+                        <img x-show="image != ''" x-bind:src=" image != ''? image : './assets/logosmansara.png'" alt="title">
+                    </div>
+                    <div>
+                        <br x-show="image != ''">
+                        <div class="line">
+                        </div>
+                        <h3 x-text="title"></h3>
+                        <hr class="mb-2">
+                        <p x-html=body></p>
+                    </div>
+                </div>
             </div>
 
-            <div x-data="{id:'',title:'',body:'',time:'',image:'',show:'false'}">
-                <div x-show="show == 'true'" class="card mb-8 md:mx-2 lg:mx-4 rounded-xl bg-white border-blue-400 border-4 shadow-lg">
-                    <div class="flex justify-between w-full p-1">
-                        <p x-text="time"></p>
-                        <button x-on:click="show = 'false', id=''" class="bg-red-500 hover:bg-red-600 text-white text-center w-7 h-7 rounded-full">x</button>
+            
+            <div class="card-box">
+            
+                @foreach ($berita as $item)    
+                <div x-bind:class="id == {{ $item['id']}} ? ' bg-white shadow-xl border'  : 'bg-white shadow border'" class="card relative">
+                    <div class="w-full flex justify-center">
+                        <img src="{{ $item->image != ''? $item->image : './assets/mdlogosmansara.jpg' }}" alt="{{ $item->title }}" >
                     </div>
-                    <div class=" p-4 md:px-6 md:pb-6">
-                        <div class="w-full flex justify-center">
-                            <img x-show="image != ''" x-bind:src=" image != ''? image : './assets/logosmansara.png'" alt="title">
+                    <div class="text">
+                        
+                        {{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}
+                        <div class="line">
                         </div>
-                        <div>
-                            <br x-show="image != ''">
-                            <div class="line">
-                            </div>
-                            <h3 x-text="title"></h3>
-                            <hr class="mb-2">
-                            <p x-html=body></p>
-                        </div>
+                        <h3 class="pb-4">
+                            {{ $item->title }}
+                        </h3>
+                        <button x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',image = '{{$item['image']}}',time = '{{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}',show='true';scrollToElement('#bacaberita')" class="absolute bottom-2">Continue Reading</button>
                     </div>
                 </div>
+                @endforeach
                 
-                <hr class="mb-2">
-                
-                <div class="card-box">
-                
-                    @foreach ($berita as $item)    
-                    <div x-bind:class="id == {{ $item['id']}} ? 'border-blue-400 border-4'  : ''" class="card">
-                        <div class="w-full flex justify-center">
-                            <img src="{{ $item->image != ''? $item->image : './assets/mdlogosmansara.jpg' }}" alt="{{ $item->title }}" >
-                        </div>
-                        <div class="text">
-                            
-                            {{ $item->updated_at }}
-                            <div class="line">
-                            </div>
-                            <h3>
-                                {{ $item->title }}
-                            </h3>
-                            <button x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',image = '{{$item['image']}}',time = '{{$item['updated_at']}}',show='true'"  onclick="window.location.href='#bacaberita'">Continue Reading</button>
-                        </div>
-                    </div>
-                    @endforeach
-                    
-                </div>
             </div>
+            
         </div>
-    </section>
+    </div>
+</section>
 
 @endsection
--- resources/views/content/pengumuman.blade.php
+++ resources/views/content/pengumuman.blade.php
@@ -2,47 +2,48 @@
 
 @section('content')
 
-    <section id="pengumuman" class="bg-white">
-        <div class="container min-h-[75vh]">
-            <div class="title">
-                <h2 id="bacapengumuman">Pengumuman</h2>
-                <p>
-                    Pengumuman SMA Negeri 1 Jepara
-                </p>
-            </div>
-
-            <div x-data="{id:'',title:'',body:'',time:'',show:'0'}" class="w-full flex justify-center flex-col gap-4">
-                <div class="w-full flex justify-center">
-                    <div x-show="show == '1'" class="card border-4 border-blue-700">
-                        <div class="flex justify-end w-full py-0 h-auto">
-                            <button x-on:click="show = '0', id=''" class="bg-red-500 hover:bg-red-600 text-white text-center w-7 h-7 rounded-full">x</button>
-                        </div>
-                        <hr>
-                        <div class="text-card w-full">
-                            <h1 x-text="title" class="text-center"></h1>
-                            <p x-html='body'></p>
-                        </div>
+<section id="pengumuman" class="bg-gray-100 pb-24">
+    <div class="container">
+        <div class="title">
+            <h2 id="bacapengumuman">Pengumuman</h2>
+            <p>
+                Pengumuman terbaru SMA Negeri 1 Jepara
+            </p>
+        </div>
+        
+        <div x-data="{id:'',title:'',body:'',time:'',show:'0'}" class="w-full flex justify-center flex-col gap-4">
+            <div class="w-full flex justify-center">
+                <div x-show="show == '1'" x-bind:class="show == '1'? 'card border shadow-xl' : 'hidden'">
+                    <div class="flex justify-end w-full py-0 h-auto">
+                        <button x-on:click="show = '0', id=''" class="bg-red-500 hover:bg-red-600 text-white text-center w-7 h-7 rounded-full">x</button>
+                    </div>
+                    <hr>
+                    <div class="text-card w-full">
+                        <h1 x-text="title" class="text-center"></h1>
+                        <p x-html='body'></p>
                     </div>
                 </div>
-                    
-                <hr class="mb-2">
-                <div class="card-box">
-
-                    @foreach ($pengumuman as $item)
-                        <button class="card" x-bind:class="id == {{ $item->id }} ? 'border-4 border-blue-700' : ''" x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',time = '{{$item['updated_at']}}',show='1'"  onclick="window.location.href='#bacapengumuman'">
-                            <div class="text-card">
-                                <h1>{{ $item->title }}</h1>
-
-                                <p class="date">
-                                    {{ $item->updated_at }}
-                                </p>
-                            </div>
-                        </button>
-                    @endforeach
-                </div>
-
             </div>
+                
+            <hr class="mb-2">
+            <div class="card-box">
+
+                @foreach ($pengumuman as $item)
+                    <button class="card shadow-[14px_22px_52px_-12px_rgba(127,_127,_127,_0.13)] items-start hover:drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" x-bind:class="id == {{ $item->id }} ? 'drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] shadow-xl' : ''" x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',time = '{{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}',show='1';scrollToElement('#bacapengumuman')">
+                        <div class="text-card">
+                            <h1>{{ $item->title }}</h1>
+
+                            <p class="date">
+                                {{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}
+
+                            </p>
+                        </div>
+                    </button>
+                @endforeach
+            </div>
+
         </div>
-    </section>
+    </div>
+</section>
 
 @endsection
