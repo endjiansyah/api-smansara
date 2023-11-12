3c33d21d06fd806d9ddd2df189246089e5591331
endjiansyah <endji1507@gmail.com>
2023-11-12 18:28:02 +0700

    add animation berita page

--- resources/views/content/berita.blade.php
+++ resources/views/content/berita.blade.php
@@ -6,7 +6,7 @@
 @endphp
 @section('content')
 
-<section id="berita" class="py-40 bg-gray-100">
+<section id="berita" class="py-28 min-h-[70vh]">
     <div class="container">
         <div class="title">
             <h2>Berita</h2>
@@ -15,8 +15,37 @@
 
         <div x-data="{id:'',title:'',body:'',time:'',image:'',show:'false'}">
             
+            <div x-show="show =='false'" class="card-box"
+            x-transition:enter="transition ease-out duration-500"
+            x-transition:enter-start="opacity-0 scale-90"
+            x-transition:enter-end="opacity-100 scale-100">
             
-            <div x-show="show == 'true'" class="card my-8 md:mx-2 lg:mx-4 rounded-xl bg-white border shadow-xl" id="bacaberita">
+                @foreach ($berita as $item)    
+                <div x-bind:class="id == {{ $item['id']}} ? ' bg-white shadow-xl border'  : 'bg-white shadow border'" class="card relative" x-show="show =='false'" 
+                x-transition:enter="transition ease-out duration-500"
+                x-transition:enter-start="opacity-0 scale-90"
+                x-transition:enter-end="opacity-100 scale-100">
+                    <div class="w-full flex justify-center">
+                        <img src="{{ $item->image != ''? $item->image : './assets/mdlogosmansara.jpg' }}" alt="{{ $item->title }}" >
+                    </div>
+                    <div class="text">
+                        
+                        {{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}
+                        <div class="line">
+                        </div>
+                        <h3 class="pb-4">
+                            {{ $item->title }}
+                        </h3>
+                        <button x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',image = '{{$item['image']}}',time = '{{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}',show='true';scrollToElement('#bacaberita')" class="absolute bottom-4">Continue Reading</button>
+                    </div>
+                </div>
+                @endforeach
+                
+            </div>
+
+            <div x-show="show == 'true'" class="card my-8 md:mx-2 lg:mx-4 rounded-xl bg-white border shadow-xl" id="bacaberita" x-transition:enter="transition ease-out duration-500"
+            x-transition:enter-start="opacity-0 scale-90"
+            x-transition:enter-end="opacity-100 scale-100">
 
                 <div class="flex justify-between w-full p-4">
                     <p x-text="time"></p>
@@ -35,29 +64,6 @@
                         <p x-html=body></p>
                     </div>
                 </div>
-            </div>
-
-
-            <div class="card-box">
-            
-                @foreach ($berita as $item)    
-                <div x-bind:class="id == {{ $item['id']}} ? ' bg-white shadow-xl border'  : 'bg-white shadow border'" class="card relative">
-                    <div class="w-full flex justify-center">
-                        <img src="{{ $item->image != ''? $item->image : './assets/mdlogosmansara.jpg' }}" alt="{{ $item->title }}" >
-                    </div>
-                    <div class="text">
-                        
-                        {{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}
-                        <div class="line">
-                        </div>
-                        <h3 class="pb-4">
-                            {{ $item->title }}
-                        </h3>
-                        <button x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',image = '{{$item['image']}}',time = '{{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}',show='true';scrollToElement('#bacaberita')" class="absolute bottom-2">Continue Reading</button>
-                    </div>
-                </div>
-                @endforeach
-                
             </div>
             
         </div>
