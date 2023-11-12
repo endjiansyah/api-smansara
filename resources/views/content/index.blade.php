ca859ef4edfeb8c55a51cd3ab741a4d541ca3d6d
endjiansyah <endji1507@gmail.com>
2023-11-12 18:21:36 +0700

    add animation pengumuman index

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -19,13 +19,13 @@
             <div x-data="{id:'',title:'',body:'',time:'',image:'',show:'false'}">
                 
                 <div x-show="show =='false'" class="card-box"
-                x-transition:enter="transition ease-out duration-300 delay-100"
+                x-transition:enter="transition ease-out duration-500"
                 x-transition:enter-start="opacity-0 scale-90"
                 x-transition:enter-end="opacity-100 scale-100">
                 
                     @foreach ($berita as $item)    
                     <div x-bind:class="id == {{ $item['id']}} ? ' bg-white shadow-xl border'  : 'bg-white shadow border'" class="card relative" x-show="show =='false'" 
-                    x-transition:enter="transition ease-out duration-300 delay-100"
+                    x-transition:enter="transition ease-out duration-500"
                     x-transition:enter-start="opacity-0 scale-90"
                     x-transition:enter-end="opacity-100 scale-100">
                         <div class="w-full flex justify-center">
@@ -46,7 +46,7 @@
                     
                 </div>
 
-                <div x-show="show == 'true' " class="card my-8 md:mx-2 lg:mx-4 rounded-xl bg-white border shadow-xl" id="bacaberita" x-transition:enter="transition ease-out duration-300 delay-100"
+                <div x-show="show == 'true'" class="card my-8 md:mx-2 lg:mx-4 rounded-xl bg-white border shadow-xl" id="bacaberita" x-transition:enter="transition ease-out duration-500"
                 x-transition:enter-start="opacity-0 scale-90"
                 x-transition:enter-end="opacity-100 scale-100">
 
@@ -122,11 +122,14 @@
             
             <div x-data="{id:'',title:'',body:'',time:'',show:'0'}" class="w-full flex justify-center flex-col gap-4">
                 <div class="w-full flex justify-center">
-                    <div x-show="show == '1'" x-bind:class="show == '1'? 'card border shadow-xl' : 'hidden'">
-                        <div class="flex justify-end w-full py-0 h-auto">
-                            <button x-on:click="show = '0', id=''" class="bg-red-500 hover:bg-red-600 text-white text-center w-7 h-7 rounded-full">x</button>
-                        </div>
-                        <hr>
+                    <div x-show="show == '1'"  
+                    x-transition:enter="transition ease-out duration-500"
+                    x-transition:enter-start="opacity-0 scale-90"
+                    x-transition:enter-end="opacity-100 scale-100" x-bind:class="show == '1'? 'card border shadow-xl' : 'hidden'">
+                    <div class="flex justify-between w-full">
+                        <p x-text="time"></p>
+                        <button x-on:click="show = '0', id=''" class="bg-red-500 hover:bg-red-600 text-white text-center w-7 h-7 rounded-full">x</button>
+                    </div>
                         <div class="text-card w-full">
                             <h1 x-text="title" class="text-center"></h1>
                             <p x-html='body'></p>
@@ -134,13 +137,18 @@
                     </div>
                 </div>
                     
-                <hr class="mb-2">
-                <div class="card-box">
+                <div class="card-box" x-show="show !=1" 
+                x-transition:enter="transition ease-out duration-500"
+                x-transition:enter-start="opacity-0 scale-90"
+                x-transition:enter-end="opacity-100 scale-100">
 
                     @foreach ($pengumuman as $item)
-                        <button class="card shadow-[14px_22px_52px_-12px_rgba(127,_127,_127,_0.13)] items-start hover:drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]" x-bind:class="id == {{ $item->id }} ? 'drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] shadow-xl' : ''" x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',time = '{{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}',show='1';scrollToElement('#bacapengumuman')">
+                        <button class="group card shadow-[14px_22px_52px_-12px_rgba(127,_127,_127,_0.13)] items-start hover:drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:bg-white duration-300" x-bind:class="id == {{ $item->id }} ? 'drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] shadow-xl' : ''" x-on:click="id='{{ $item['id'] }}',body = '{{$item['body']}}',title = '{{$item['title']}}',time = '{{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}',show='1';scrollToElement('#bacapengumuman')">
                             <div class="text-card">
-                                <h1>{{ $item->title }}</h1>
+                                <div>
+                                    <h1>{{ $item->title }}</h1>
+                                    <div class="w-0 group-hover:w-20 duration-300 h-1 bg-gray-100 group-hover:bg-gray-400"></div>
+                                </div>
 
                                 <p class="date">
                                     {{ $item->updated_at->isoFormat('dddd, D MMMM Y') }}
